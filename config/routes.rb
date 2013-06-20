5593f4c070f51b55923896944bf87d0788b8d12b
MakerSquare Student <student@lab-3.att.net>
2013-06-20 16:02:15 -0500

    Add picture to list of ideas.

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Ideator::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => '#index'
+  root :to => 'ideas#index'
 
   # See how all your routes lay out with "rake routes"
 
